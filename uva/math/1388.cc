ff0f5546d968110df14b5527f393ceedef31c84c
Wang Jing <wangjild@gmail.com>
2013-11-10 20:04:50 +0800

    commit

---
+++ uva/math/1388.cc
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cmath>
+
+int main () {
+	int n, m;
+
+	while (scanf("%d%d", &n, &m) == 2) {
+		double ans = 0.0;
+		for (int i = 1; i < n; ++i) {
+			double pos = (double) i / n * (n + m);
+			ans += fabs(pos - floor(pos + 0.5)) / (n + m);
+		}
+
+		printf("%.4lf\n", ans * 10000);
+	}
+	return 0;
+}
